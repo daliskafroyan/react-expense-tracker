4724661a4c783f8926ca7d958cf77601da559aaa
Daliska F. Royan <18845506030@163.com>
2020-06-13 09:46:14 +0700

    Updates

---
+++ precache-manifest.8d14d3c42c4f4d134e7dc1a36a97c09d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd5b2750aa8e2800ce4e15861996722f",
+    "url": "/react-expense-tracker/index.html"
+  },
+  {
+    "revision": "4859841423c082b1cb61",
+    "url": "/react-expense-tracker/static/css/main.b13b746d.chunk.css"
+  },
+  {
+    "revision": "f5c59187d527867d4931",
+    "url": "/react-expense-tracker/static/js/2.f49cac5b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-expense-tracker/static/js/2.f49cac5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4859841423c082b1cb61",
+    "url": "/react-expense-tracker/static/js/main.4db45873.chunk.js"
+  },
+  {
+    "revision": "4752dbf25a34323b5b7c",
+    "url": "/react-expense-tracker/static/js/runtime-main.38b51ddc.js"
+  }
+]);
